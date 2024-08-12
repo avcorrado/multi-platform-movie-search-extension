ee04b150ee0738752a74b0889c7902e3b47d373c
Andrea-V Corrado <andrea-v.corrado@db.com>
2024-08-12 17:27:37 +0200

    chore: files changed background_script.js icon.png manifest.json

---
+++ background_script.js
@@ -0,0 +1,47 @@
+function createContextMenu() {
+  browser.contextMenus.create({
+    id: 'searchIMDb',
+    title: "Search IMDb for '%s'",
+    contexts: ['selection'],
+  });
+
+  browser.contextMenus.create({
+    id: 'searchTrakt',
+    title: "Search Trakt for '%s'",
+    contexts: ['selection'],
+  });
+
+  browser.contextMenus.create({
+    id: 'searchLetterboxd',
+    title: "Search Letterboxd for '%s'",
+    contexts: ['selection'],
+  });
+}
+
+function handleContextMenuClick(info) {
+  const query = info.selectionText;
+
+  switch (info.menuItemId) {
+    case 'searchIMDb':
+      browser.tabs.create({
+        url: `https://www.imdb.com/find?q=${encodeURIComponent(query)}`,
+      });
+      break;
+
+    case 'searchTrakt':
+      browser.tabs.create({
+        url: `https://trakt.tv/search?query=${encodeURIComponent(query)}`,
+      });
+      break;
+
+    case 'searchLetterboxd':
+      browser.tabs.create({
+        url: `https://letterboxd.com/search/${encodeURIComponent(query)}/`,
+      });
+      break;
+  }
+}
+
+// Add listeners
+browser.runtime.onInstalled.addListener(createContextMenu);
+browser.contextMenus.onClicked.addListener(handleContextMenuClick);
